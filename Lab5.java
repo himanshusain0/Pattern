7541060f0851016205948ad62b6e0833b5309d94
Himanshu kumar <112979593+himanshusain0@users.noreply.github.com>
2023-05-10 12:35:09 +0530

    Add files via upload

---
+++ Lab5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab5 {
+     public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);
+      int n=sc.nextInt();
+      int min=0;
+      for(int i=1;i<=2*n-1;i++){
+            for(int j=1;j<=2*n-1;j++){
+                  int a=i;
+                  if(i>n)
+                  a=2*n-i;
+                  int b=j;
+                  if(b>n)
+                  b=2*n-j;
+                  if(a<b)
+                  min=a;
+                  else min=b;
+                  System.out.print(n+1-min);
+            }
+            System.out.println();
+      } 
+     }
+
+}
